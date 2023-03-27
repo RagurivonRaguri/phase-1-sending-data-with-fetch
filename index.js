3a5d97c9bff4c7403b07497012c35ae4f9ab3a21
RagurivonRaguri <lincolnmuraguri93@gmail.com>
2023-03-27 20:04:29 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Add your code here
+function submitData(name, email) {
+    let formData = {
+      name: name,
+      email: email
+    };
+  
+    let configObj = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify(formData)
+    };
+  
+    return fetch("http://localhost:3000/users", configObj)
+      .then(function(res) {
+        return res.json();
+      })
+      .then(function(data) {
+        let newId = data.id;
+        let idText = document.createTextNode(newId);
+        document.body.appendChild(idText);
+      })
+      .catch(function(error) {
+        let errorText = document.createTextNode(error.message);
+        document.body.appendChild(errorText);
+      });
+  }
